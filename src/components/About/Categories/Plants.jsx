9f744b3dc6e034d849bb3bf432d3d82d2d91c677
gabo <gabi.280795@gmail.com>
2023-10-29 20:55:51 -0300

    agregado de imagenes, a locales1

--- src/components/About/Categories/Plants.jsx
+++ src/components/About/Categories/Plants.jsx
@@ -8,7 +8,7 @@ export function Plants() {
         marginBottom : ''
     }
     const urlImg2 ={
-        url : "https://www.acozzuol.com.ar/eng/img/quienes_somos_planta02.png"
+        url : "/images/about-us/quienes_somos_planta02.png"
     }
     if (window.matchMedia('(max-width: 768px)').matches){
         /* styles.display = 'none' */
@@ -17,7 +17,7 @@ export function Plants() {
     }
     return (
         <>
-            <img style={styles} src="https://www.acozzuol.com.ar/img/quienes_somos_planta01.png" alt="" />
+            <img style={styles} src="/images/about-us/quienes_somos_planta01.png" alt="" />
             <img style={styles2} src={urlImg2.url} alt="" />
         </>
     )
